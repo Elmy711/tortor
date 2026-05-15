48ed64da6a5cbe6215b370574908a98aaf4d2ffa
elmiya0711 <elmiya0711@gmail.com>
2026-05-15 13:27:43 +0700

    update

--- tortor.py
+++ tortor.py
@@ -13,22 +13,21 @@ def main():
                 print u'{}[!]{} Tor is not installed. Exiting...'.format(color.RED, color.END)
                 sys.exit(1)
             else:
-                # Initial timestamp and increment the counter
+                
                 start_time = datetime.datetime.now().time().strftime('%H:%M:%S')
                 counter += 1
 
-                # Baca proxy dari file
+                
                 with open('proxy.txt', 'r') as f:
-                    proxies = [line.strip() for line in f if line.strip()]
-                proxy = random.choice(proxies)
-                proxies = {
-                    'http': proxy,
-                    'https': proxy
+    proxies = [line.strip() for line in f if line.strip()]
+proxy = random.choice(proxies)
+session = tor.new_session(proxies={'http': proxy, 'https': proxy})
+print('{}[!]{} New Tor session initialized...'.format(color.BLUE, color.END))
                 }
 
-                # Init a new Tor session dengan proxy
+                
                 session = tor.new_session(proxies=proxies)
                 print u'{}[!]{} New Tor session initialized...'.format(color.BLUE, color.END)
                 print u'\n{}[+]{} Target: {}{}{}'.format(color.PURPLE, color.END, color.PURPLE, target, color.END)
 
-                # Getting
+                
